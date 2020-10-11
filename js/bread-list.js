f686bf4061732eb20f8489d1a62cc4ab12825f15
jungmiyoung <myjung93a@gmail.com>
2020-10-11 16:21:30 +0900

    SEARCH : JS FINISH

--- js/bread-list.js
+++ js/bread-list.js
@@ -31,6 +31,19 @@ function onClickEventProduct(items) {
     const value = dataset.value;
     onButtonItems(event, value);
   });
+
+  const searchBtn = document.querySelector('.search__button');
+  searchBtn.addEventListener('click',(event) =>{
+    onSearchProduct(event, items);
+    
+  });
+
+  const searchInput = document.querySelector(".search__input");
+  searchInput.addEventListener('keypress', (event) =>{
+    if(event.key ==="Enter") {
+      onSearchProduct(event, items);
+    }
+  });
 }
 
 function onButtonItems(event, value) {
@@ -60,10 +73,48 @@ function onButtonItems(event, value) {
 loadItem().then((products) => {
   showProducts(products);
   onClickEventProduct(products);
+  onSearchProduct(products);
 });
 
-const searchInput = document.querySelector(".search__input");
+function onSearchProduct(event,items) {
+    const itemList =document.querySelectorAll('.item__wrap');
+    const searchInput = document.querySelector(".search__input");
+    const eachWords = searchInput.value.trim();
+    breadItem.classList.remove("animation");
+      itemList.forEach(item => {
+      breadItem.classList.add("animation");
 
-searchInput.addEventListener("input", () => {
-  console.log(searchInput.value.length);
+        const itemTitle=item.querySelector('.dessert__explain').textContent;
+        if(itemTitle.indexOf(eachWords)>-1){
+          item.classList.remove('invisible');
+        }else{
+          item.classList.add('invisible');
+        }
+
+       });
+    breadItem.classList.remove("animation");
+    onDeleteItem();
+    searchInput.value='';
+    searchInput.focus();
+}
+
+let idNum =0;
+const previewInput = document.querySelector('.preview__input');
+
+function onDeleteItem() {
+  const searchInput = document.querySelector(".search__input");
+  previewInput.innerHTML+=
+  `<li data-id="${idNum}"><span>${searchInput.value}</span>
+  <button class="search__delete"><i class="fas fa-times" data-id="${idNum}"></i></button></li>`;
+  idNum++;
+
+}
+
+previewInput.addEventListener('click', (e) =>{
+  const dataId = e.target.dataset.id;
+  if(dataId) {
+    const dataDeleted = document.querySelector(`.preview__input > li[data-id='${idNum}']`);
+    dataDeleted.remove();
+  }
+ 
 });
